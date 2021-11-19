3c691c2b5f210d19c3ef4f329f263a4f4ad903ed
Frank Jennings <frank@jenningsnet.net>
2021-11-18 19:19:08 -0800

    Updates

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm ~/.zshrc
+rm ~/.zsh_aliases
+rm ~/.p10k.zsh
+rm ~/.tmux.conf
+
+ln -s ~/configs/.zshrc ~/.zshrc
+ln -s ~/configs/.zsh_aliases ~/.zsh_aliases
+ln -s ~/configs/.p10k.zsh ~/.p10k.zsh
+ln -s ~/configs/.tmux.conf ~/.tmux.conf:q
